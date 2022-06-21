27dbc5f7283134f795407730e84d9c8739da09b4
Erik <Erik.quintana@icloud.com>
2022-06-21 15:31:01 -0600

    add pet card

--- src/components/PetsList.js
+++ src/components/PetsList.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PetCard from "./PetCard";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 function PetsList({ puppies, isLoaded }) {
-  function CircularUnderLoad() {
-    return <CircularProgress disableShrink />;
-  }
 
-  const puppiesItems = puppies.map((puppy) => {
-    return (
-      <Link
-        exact
-        to={`/pets-list/${puppy.id}`}
-        className="puppy-card"
-        key={puppy.id}
-      >
-        <h2>{puppy.name}</h2>
-        <img src={puppy.image} alt={puppy.breed} />
-      </Link>
-    );
-  });
 
-  return (
-    <div>
-      <main>
-        <div id="content">{isLoaded ? puppiesItems : CircularUnderLoad()}</div>
-      </main>
-    </div>
-  );
-}
+  const puppiesItems = puppies.map((puppy) => <PetCard key={puppy.id} puppy={puppy}/>);
+  return(
+   <main> 
+      <div className="content">{puppiesItems}</div> 
+   </main>
+ 
+  )
+    
+};
+
+
 
 export default PetsList;
