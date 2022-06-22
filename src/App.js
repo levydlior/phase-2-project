139c098d069cea63cae09cdffd897bf3508e1930
Lucas Sweeney <lsweeney0124@gmail.com>
2022-06-22 15:34:11 -0500

    added delete function to adopt button

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Form from "./components/Form";
 import AdoptionForm from "./components/AdoptionForm";
 import FavoritePets from "./components/FavoritePets";
 
-function App() {
-  const [puppies, setPuppies] = useState([
+function App () {
+  const [ puppies, setPuppies ] = useState([
     {},
     {},
     {},
@@ -21,8 +21,8 @@ function App() {
     {},
     {},
   ]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [canAdopt, setCanAdopt] = useState(false);
+  const [ isLoaded, setIsLoaded ] = useState(false);
+  const [ canAdopt, setCanAdopt ] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:3001/pets")
@@ -33,16 +33,16 @@ function App() {
       });
   }, []);
 
-  function handleSubmit(newPet) {
-    const updatedPets = [...puppies, newPet];
+  function handleSubmit (newPet) {
+    const updatedPets = [ ...puppies, newPet ];
     setPuppies(updatedPets);
   }
 
-  function handleAdoptionFormSubmit() {
+  function handleAdoptionFormSubmit () {
     setCanAdopt(true);
   }
 
-  function handleLikedPet(petObject) {
+  function handleLikedPet (petObject) {
     const updatedPets = puppies.map((pup) => {
       if (pup.id === petObject.id) {
         return petObject;
@@ -60,22 +60,22 @@ function App() {
       <Header />
       <Switch>
         <Route path="/surrender-pet">
-          <Form onPetSubmit={handleSubmit} />
+          <Form onPetSubmit={ handleSubmit } />
         </Route>
         <Route path="/favorite-pets">
-          <FavoritePets likedPets={likedPets} isLoaded={isLoaded} />
+          <FavoritePets likedPets={ likedPets } isLoaded={ isLoaded } />
         </Route>
         <Route exact path="/pets-list/:id">
-          <PetDetails canAdopt={canAdopt} onLikePet={handleLikedPet} />
+          <PetDetails canAdopt={ canAdopt } onLikePet={ handleLikedPet } setPuppies={ setPuppies } puppies={ puppies } />
         </Route>
         <Route path="/add-pet">
           <h1>Form to add / edit</h1>
         </Route>
         <Route exact path="/adoption-form">
-          <AdoptionForm onAdoptionFormSubmit={handleAdoptionFormSubmit} />
+          <AdoptionForm onAdoptionFormSubmit={ handleAdoptionFormSubmit } />
         </Route>
         <Route exact path="/">
-          <PetsList puppies={puppies} isLoaded={isLoaded} />
+          <PetsList puppies={ puppies } isLoaded={ isLoaded } />
         </Route>
         <Route path="*">
           <h1>404 Page Not Found!</h1>
